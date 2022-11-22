03ef021b21bfa1fb4dbf5bfeed65af8662844ad9
Maksim Meleshko <mel2112@mail.ru>
2022-11-22 23:41:23 +0300

    add layouts with scrollview and items

--- app/src/main/java/com/meleshkomd/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/meleshkomd/shoppinglist/presentation/MainActivity.kt
@@ -2,19 +2,48 @@ package com.meleshkomd.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.meleshkomd.shoppinglist.R
+import com.meleshkomd.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var llShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("MainActivity", it.toString())
+            showList(it)
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llShopList.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            // Для создания из макета view используем LI и inflate
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnabledState(shopItem)
+                true
+            }
+            llShopList.addView(view)
         }
     }
 }
